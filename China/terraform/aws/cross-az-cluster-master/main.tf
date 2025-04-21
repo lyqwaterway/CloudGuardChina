b10459935769618265588a40e92271f96e41abf5
yongqiangliu <343847055@qq.com>
2025-04-21 15:24:37 +0800

    add cross cluster template for aws China

--- China/terraform/aws/cross-az-cluster-master/main.tf
+++ China/terraform/aws/cross-az-cluster-master/main.tf
@@ -21,25 +21,30 @@ resource "aws_route_table" "private_subnet_rtb" {
     Name = "Private Subnets Route Table"
   }
 }
-resource "aws_route_table_association" "private_rtb_to_private_subnets" {
+resource "aws_route_table_association" "private_rtb_to_private_subnets_a" {
   depends_on = [module.launch_vpc, aws_route_table.private_subnet_rtb]
   route_table_id = aws_route_table.private_subnet_rtb.id
   subnet_id = module.launch_vpc.private_subnets_ids_list[0]
 }
+resource "aws_route_table_association" "private_rtb_to_private_subnets_b" {
+  depends_on = [module.launch_vpc, aws_route_table.private_subnet_rtb]
+  route_table_id = aws_route_table.private_subnet_rtb.id
+  subnet_id = module.launch_vpc.private_subnets_ids_list[1]
+}
+
 module "launch_cluster_into_vpc" {
-  source = "../cluster"
+  source = "../cross-az-cluster"
   providers = {
     aws = aws
   }
 
   vpc_id = module.launch_vpc.vpc_id
-  public_subnet_id = module.launch_vpc.public_subnets_ids_list[0]
-  private_subnet_id = module.launch_vpc.private_subnets_ids_list[0]
+  public_subnet_ids = module.launch_vpc.public_subnets_ids_list
+  private_subnet_ids = module.launch_vpc.private_subnets_ids_list
   private_route_table = aws_route_table.private_subnet_rtb.id
   gateway_name = var.gateway_name
   gateway_instance_type = var.gateway_instance_type
   key_name = var.key_name
-  allocate_and_associate_eip = var.allocate_and_associate_eip
   volume_size = var.volume_size
   volume_encryption = var.volume_encryption
   enable_instance_connect = var.enable_instance_connect
@@ -50,10 +55,10 @@ module "launch_cluster_into_vpc" {
   gateway_version = var.gateway_version
   admin_shell = var.admin_shell
   gateway_SICKey = var.gateway_SICKey
-  gateway_password_hash = var.gateway_password_hash
-  gateway_maintenance_mode_password_hash = var.gateway_maintenance_mode_password_hash
   memberAToken = var.memberAToken
   memberBToken = var.memberBToken
+  gateway_password_hash = var.gateway_password_hash
+  gateway_maintenance_mode_password_hash = var.gateway_maintenance_mode_password_hash
   resources_tag_name = var.resources_tag_name
   gateway_hostname = var.gateway_hostname
   allow_upload_download = var.allow_upload_download
@@ -61,4 +66,5 @@ module "launch_cluster_into_vpc" {
   gateway_bootstrap_script = var.gateway_bootstrap_script
   primary_ntp = var.primary_ntp
   secondary_ntp = var.secondary_ntp
+  volume_type = var.volume_type
 }
--- China/terraform/aws/cross-az-cluster/terraform.tfvars
+++ China/terraform/aws/cross-az-cluster/terraform.tfvars
@@ -1,20 +1,15 @@
 //PLEASE refer to README.md for accepted values FOR THE VARIABLES BELOW
 
 // --- VPC Network Configuration ---
-vpc_cidr = "10.0.0.0/16"
-public_subnets_map = {
-  "cn-north-1a" = 1
-}
-private_subnets_map = {
-  "cn-north-1a" = 2
-}
-subnets_bit_length = 8
+vpc_id = "vpc-12345678"
+public_subnet_ids = ["subnet-abc123", "subnet-def456"]
+private_subnet_ids = ["subnet-abc234", "subnet-def567"]
+private_route_table = "rtb-12345678"
 
 // --- EC2 Instance Configuration ---
 gateway_name = "Check-Point-Cluster-tf"
-gateway_instance_type = "c5.xlarge"
-key_name = "cn-publickey"
-allocate_and_associate_eip = true
+gateway_instance_type = "c6in.xlarge"
+key_name = "publickey"
 volume_size = 100
 volume_encryption = "alias/aws/ebs"
 enable_instance_connect = false
@@ -29,9 +24,9 @@ predefined_role = ""
 // --- Check Point Settings ---
 gateway_version = "R81.20-BYOL"
 admin_shell = "/etc/cli.sh"
-gateway_SICKey = "123456789"
-gateway_password_hash = "$6$Ue.PCqZMxS3hM01u$Byt.LpmBLN6TYn38obCc.kwubzHwcXtxvLl34iHbNER/suoLYJbkYCB5b8mWBq7JxIEz52.QyQJDelcWwefyq1"
-gateway_maintenance_mode_password_hash = "grub.pbkdf2.sha512.10000.C9D492E201B2F6729E9828F3B92BD2B39F2961ED7F848CD96C86643D4CF53A7AF38B23F2B95FFBCC97AFECA4393EC045A6EB2DFFFA937C094E71ABF828936401.4913885D116A9085C1D652A696CDD52D4D7F28B1B626853126934E47E7DCD25D2D51B75EBF0C89F74916FF07AADE1044E519B081EA77D5B51AFBFB9F2CC65E18" # For R81.10 and below the gateway_password_hash is used also as maintenance-mode password.
+gateway_SICKey = "12345678"
+gateway_password_hash = ""
+gateway_maintenance_mode_password_hash = "" # For R81.10 and below the gateway_password_hash is used also as maintenance-mode password.
 
 // --- Quick connect to Smart-1 Cloud (Recommended) ---
 memberAToken = ""
